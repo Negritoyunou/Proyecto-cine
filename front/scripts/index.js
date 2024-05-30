bc4b355ce9e3249c0060fd27b20610c617059dba
Ignacio Arevalo <nachoarevalo11@gmail.com>
2024-05-30 19:47:12 -0300

    Entrega Hw 3 M2 Bootstrap

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -5,40 +5,54 @@ const movieList = document.querySelector('.movie-list');
 function fetchMoviesFromApi() {
   $.get('https://students-api.up.railway.app/movies')
     .done(function(moviesData) {
+
       // Crear las tarjetas de películas con los datos obtenidos
+
       const movieCards = moviesData.map(movie => {
         const movieCard = document.createElement('li');
         movieCard.className = 'movie-card';
+
         const poster = document.createElement('img');
         poster.src = movie.poster;
         poster.alt = movie.title;
         movieCard.appendChild(poster);
+
         const info = document.createElement('div');
         info.className = 'movie-info';
+
         const title = document.createElement('h3');
         title.textContent = movie.title;
         title.style.fontWeight = 'bold';
         info.appendChild(title);
+
         const details = document.createElement('p');
         details.textContent = `${movie.year} | ${movie.duration}`;
         info.appendChild(details);
+
         const director = document.createElement('p');
         director.textContent = `Director: ${movie.director}`;
         info.appendChild(director);
+
         const genres = document.createElement('p');
         genres.textContent = `Géneros: ${movie.genre.join(', ')}`;
         info.appendChild(genres);
+
         const rate = document.createElement('p');
         rate.textContent = `Puntuación: ${movie.rate}`;
         info.appendChild(rate);
+
         movieCard.appendChild(info);
+        
         return movieCard;
       });
+
       // Agregar las tarjetas de películas al contenedor
       movieList.append(...movieCards);
     })
+
     .fail(function(error) {
       console.error('Error al obtener los datos:', error);
+
     });
 }
 
